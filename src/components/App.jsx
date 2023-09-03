43ed1d8eaab4dc4d3cc5349ff0743a7a6d443649
arhip14 <kruvdyk1503@gmail.com>
2023-09-03 21:13:21 +0300

    App.jsx edit, commit-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+const Home = React.lazy(() => import('./Home'));
+const Movies = React.lazy(() => import('./Movies'));
+const MovieDetails = React.lazy(() => import('./MovieDetails'));
+const Cast = React.lazy(() => import('./Cast'));
+const Reviews = React.lazy(() => import('./Reviews'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router basename='films finder'>
+      <div>
+        <Routes>
+          <Route path='/' element={Home} />
+          <Route path='/movies' element={Movies} />
+          <Route path='/movies:/movieId' element={MovieDetails} />
+          <Route path='/movies:/movieId/cast' element={Cast} />
+          <Route path='/movies:/movieId/reviews' element={Reviews} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
+
+export default App;
