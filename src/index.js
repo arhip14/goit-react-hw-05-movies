30538868ecdcc6ad8acb6ccba18004822c71f6fd
arhip14 <kruvdyk1503@gmail.com>
2023-09-04 15:25:14 +0300

    Index.js problem fix , commit-7

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <Router basename="/goit-react-hw-05-movies"> 
+  <BrowserRouter basename="/goit-react-hw-05-movies">
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
