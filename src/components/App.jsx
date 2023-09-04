b06709033a0ffd9c539fe1c9a01b50e739c674d0
arhip14 <kruvdyk1503@gmail.com>
2023-09-04 15:49:12 +0300

    fix bugs with route, commit-8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('./Home'));
 const Movies = lazy(() => import('./Movies'));
@@ -9,19 +9,17 @@ const Reviews = lazy(() => import('./Reviews'));
 
 const App = () => {
   return (
-    <Router basename="/"> {/* Оновлено basename */}
-      <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId" element={<MovieDetails />} />
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-          </Routes>
-        </Suspense>
-      </div>
-    </Router>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        </Routes>
+      </Suspense>
+    </div>
   );
 };
 
